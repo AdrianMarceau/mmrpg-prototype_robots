5bb0b38752b82872b3dc4e85590992ecd8b1358c
Ageman20XX <adrian.marceau@gmail.com>
2023-06-16 01:15:45 -0400

    Updated the Crystal Joe's functions

--- crystal-joe/functions.php
+++ crystal-joe/functions.php
@@ -15,13 +15,13 @@ $functions = array(
         extract($objects);
         
         // If this robot has the signature ability equipped, update the sprite image
-        $base_image_token = $this_robot->robot_base_image;
-        $alt_image_token = $this_robot->robot_base_image.'_alt';
+        $base_image_token = $this_robot->robot_token;
+        $alt_image_token = $this_robot->robot_token.'_alt';
         if ($this_robot->has_ability('crystal-frag') 
             && $this_robot->robot_image !== $alt_image_token){
             $this_robot->set_image($alt_image_token); 
         } elseif (!$this_robot->has_ability('crystal-frag') 
-            && $this_robot->robot_image !== $base_image_token){
+            && $this_robot->robot_image === $alt_image_token){
             $this_robot->set_image($base_image_token); 
         }
 
@@ -38,30 +38,27 @@ $functions = array(
         if ($this_robot->has_ability('crystal-frag')){
 
             // Define this ability's attachment token and info
-            $static_attachment_key = $this_robot->get_static_attachment_key();
+            $static_attachment_key = 0; //$this_robot->get_static_attachment_key();
             $this_attachment_info = rpg_ability::get_static_attachment('crystal-frag', 'crystal-frag', $static_attachment_key);
             $this_attachment_token = $this_attachment_info['attachment_token'];
 
-            // Predefine attachment create and destroy text for later
-            $this_create_text = $this_robot->print_name().' protected '.$this_robot->get_pronoun('reflexive').' with a '.rpg_type::print_span('crystal', 'Crystal Frag').'!';
-            $this_create_text .= '<br /> The fragment blocks all damage <em>once</em> before fading!';
-
             // Check if the attachment exists yet, and if not add it now
-            $attachment_already_exists = $this_battle->has_attachment($static_attachment_key, $this_attachment_token);
-            if (!$attachment_already_exists){ 
-                $this_robot->set_frame('summon');
-                $this_battle->set_attachment($static_attachment_key, $this_attachment_token, $this_attachment_info);                   
-                $this_battle->events_create($this_robot, false,
-                    $this_robot->print_name_s().' Skill',
-                    $this_create_text,
-                    array(
-                        'event_flag_camera_action' => true,
-                        'event_flag_camera_side' => $this_robot->player->player_side,
-                        'event_flag_camera_focus' => $this_robot->robot_position,
-                        'event_flag_camera_depth' => $this_robot->robot_key
-                        )
-                  );
-                $this_robot->reset_frame();
+            $attachment_already_exists = $this_robot->has_attachment($this_attachment_token);
+            if (!$attachment_already_exists){
+                //$this_battle->events_create(false, false, 'debug', 'onbattlestart');
+                $this_robot->set_attachment($this_attachment_token, $this_attachment_info);
+                if ($this_robot->robot_position === 'active'){
+                	  $this_robot->set_frame('summon');
+                    $this_battle->events_create(false, false, '', '',
+                        array(
+                            'event_flag_camera_action' => true,
+                            'event_flag_camera_side' => $this_robot->player->player_side,
+                            'event_flag_camera_focus' => $this_robot->robot_position,
+                            'event_flag_camera_depth' => $this_robot->robot_key
+                            )
+                      );
+                    $this_robot->reset_frame();                
+                }
             }
         
         }
@@ -79,30 +76,27 @@ $functions = array(
         if ($this_robot->has_ability('crystal-frag')){
 
             // Define this ability's attachment token and info
-            $static_attachment_key = $this_robot->get_static_attachment_key();
+            $static_attachment_key = 0; //$this_robot->get_static_attachment_key();
             $this_attachment_info = rpg_ability::get_static_attachment('crystal-frag', 'crystal-frag', $static_attachment_key);
             $this_attachment_token = $this_attachment_info['attachment_token'];
 
-            // Predefine attachment create and destroy text for later
-            $this_create_text = $this_robot->print_name().' protected '.$this_robot->get_pronoun('reflexive').' with a '.rpg_type::print_span('crystal', 'Crystal Frag').'!';
-            $this_create_text .= '<br /> The fragment blocks all damage <em>once</em> before fading!';
-
             // Check if the attachment exists yet, and if not add it now
-            $attachment_already_exists = $this_battle->has_attachment($static_attachment_key, $this_attachment_token);
-            if (!$attachment_already_exists){ 
-                $this_robot->set_frame('summon');
-                $this_battle->set_attachment($static_attachment_key, $this_attachment_token, $this_attachment_info);                   
-                $this_battle->events_create($this_robot, false,
-                    $this_robot->print_name_s().' Skill',
-                    $this_create_text,
-                    array(
-                        'event_flag_camera_action' => true,
-                        'event_flag_camera_side' => $this_robot->player->player_side,
-                        'event_flag_camera_focus' => $this_robot->robot_position,
-                        'event_flag_camera_depth' => $this_robot->robot_key
-                        )
-                  );
-                $this_robot->reset_frame();
+            $attachment_already_exists = $this_robot->has_attachment($this_attachment_token);
+            if (!$attachment_already_exists){
+                //$this_battle->events_create(false, false, 'debug', 'onendofturn');
+                $this_robot->set_attachment($this_attachment_token, $this_attachment_info);
+                if ($this_robot->robot_position === 'active'){
+                	  $this_robot->set_frame('summon');
+                    $this_battle->events_create(false, false, '', '',
+                        array(
+                            'event_flag_camera_action' => true,
+                            'event_flag_camera_side' => $this_robot->player->player_side,
+                            'event_flag_camera_focus' => $this_robot->robot_position,
+                            'event_flag_camera_depth' => $this_robot->robot_key
+                            )
+                      );
+                    $this_robot->reset_frame();                
+                }
             }
         
         }
