6677f3878f213a3ba0580f43e849f491b497206f
Ageman20XX <adrian.marceau@gmail.com>
2023-06-25 23:50:39 -0400

    Updated Trill's functions

--- trill/functions.php
+++ trill/functions.php
@@ -9,58 +9,99 @@ $functions = array(
         return true;
 
     },
-    'robot_function_onendofturn' => function($objects){
+    'robot_function_onability' => function($objects){
 
         // Extract all objects into the current scope
         extract($objects);
-        
+
+        // Check to see which battle strategy we should use
+        $battle_strategy = $this_robot->robot_energy < $this_robot->robot_base_energy ? 'offensive' : 'defensive';
+
+        // If the robot has NOT taken damage, we will not use offensive moves
+        if ($battle_strategy === 'offensive'){
+
+            // return empty to allow normal ability logic
+            return '';
+
+        } elseif ($battle_strategy === 'defensive'){
+
+            // manually define which abilities are allowed (start with global support)
+            $allowed_abilities = array();
+            $allowed_abilities = array_merge($allowed_abilities, rpg_ability::get_global_support_abilities());
+            return $allowed_abilities;
+
+        }
+
+        // Return true on success
+        return true;
+
+    },
+    'robot_function_onstatchange' => function($objects, $show_event = true){
+
+        // Extract all objects into the current scope
+        extract($objects);
+
         // Define the list of stats and their associated alt images
         $stats_to_alts = array(
-            'energy' => '', 
-            'attack' => '_alt', 
-            'defense' => '_alt2', 
+            'energy' => '',
+            'attack' => '_alt',
+            'defense' => '_alt2',
             'speed' => '_alt3'
-        		);
-        
+            );
+
         // Collect the robot's stats and rank them so we know which alt to use
-				$ranked_stats = array(
-            'energy' => $this_robot->robot_energy, 
-            'attack' => $this_robot->robot_attack, 
-            'defense' => $this_robot->robot_defense, 
+        $ranked_stats = array(
+            'energy' => $this_robot->robot_energy,
+            'attack' => $this_robot->robot_attack,
+            'defense' => $this_robot->robot_defense,
             'speed' => $this_robot->robot_speed
-        		);       
+            );
         asort($ranked_stats);
         $ranked_stats = array_reverse($ranked_stats, true);
         $max_value = reset($ranked_stats);
         $best_stats = array();
         foreach($ranked_stats as $stat => $value){
-            if ($value == $max_value){ $best_stats[] = $stat; } 
+            if ($value == $max_value){ $best_stats[] = $stat; }
             else { break; }
         }
-				$best_stat = $best_stats[array_rand($best_stats)];
+                $best_stat = $best_stats[array_rand($best_stats)];
         $best_stat_alt_token = $stats_to_alts[$best_stat];
-        
+
         // Define the new image given the best stat and update if necessary
         $current_image = $this_robot->robot_image;
         $required_image = $this_robot->robot_token.$best_stat_alt_token;
         if ($current_image !== $required_image){
-            $event_options = array(
-            	'event_flag_camera_action' => true,
-            	'event_flag_camera_side' => $this_player->player_side,
-            	'event_flag_camera_focus' => $this_robot->robot_position,
-            	'event_flag_camera_depth' => $this_robot->robot_key
-            	);
-            $this_robot->set_frame('summon');
-            $this_battle->events_create(false, false, '', '', $event_options);
-            $this_robot->set_frame('defend');
-        	  $this_robot->set_image($required_image);
-            $this_battle->events_create(false, false, '', '', $event_options);
-            $this_robot->reset_frame();
+            if ($show_event){
+                $event_options = array(
+                    'event_flag_camera_action' => true,
+                    'event_flag_camera_side' => $this_player->player_side,
+                    'event_flag_camera_focus' => $this_robot->robot_position,
+                    'event_flag_camera_depth' => $this_robot->robot_key
+                    );
+                $this_robot->set_frame('summon');
+                $this_battle->events_create(false, false, '', '', $event_options);
+                $this_robot->set_frame('defend');
+                $this_robot->set_image($required_image);
+                $this_battle->events_create(false, false, '', '', $event_options);
+                $this_robot->reset_frame();
+            } else {
+                $this_robot->set_image($required_image);
+            }
         }
-        
+
         // Return true on success
         return true;
 
     }
 );
+$functions['robot_function_onendofturn'] = function($objects) use ($functions){
+    return $functions['robot_function_onstatchange']($objects, true);
+};
+$functions['rpg-ability_stat-boost_after'] = function($objects) use ($functions){
+    return $functions['robot_function_onstatchange']($objects, false);
+};
+$functions['rpg-ability_stat-break_after'] = function($objects) use ($functions){
+    return $functions['robot_function_onstatchange']($objects, false);
+
+};
 ?>
