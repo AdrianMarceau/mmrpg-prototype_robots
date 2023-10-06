76db97c1ae932e2bdbcd1fa0dec0f2b8575a6e5f
Ageman20XX <adrian.marceau@gmail.com>
2023-10-06 19:57:28 -0400

    Updated Slur's functions

--- slur/functions.php
+++ slur/functions.php
@@ -10,6 +10,7 @@ $functions = array(
 
     },
     'robot_function_onload' => function($objects){
+        //error_log('robot_function_onload for '.$objects['this_robot']->robot_token);
 
         // Extract all objects into the current scope
         extract($objects);
@@ -77,6 +78,79 @@ $functions = array(
         $this_robot->set_quote('battle_taunt', $taunt_quote);
         $this_robot->set_quote('battle_victory', $victory_quote);
         $this_robot->set_quote('battle_defeat', $defeat_quote);
+
+        // Return true on success
+        return true;
+
+    },
+    'robot_function_onbattlestart' => function($objects){
+        //error_log('robot_function_onbattlestart() for '.$objects['this_robot']->robot_token);
+
+        // Extract all objects into the current scope
+        extract($objects);
+
+        // Update this robot's quotes to be more dynamic with the battle context
+        $context = $this_battle->values['context'];
+        //error_log('$context = '.print_r($context, true));
+        $human_player_info = !empty($this_battle->values['battle_players']['this_player']) ? $this_battle->values['battle_players']['this_player'] : array();
+        $human_player_is_visible = !empty($human_player_info['player_visible']) ? true : false;
+        $human_player_robots_count = !empty($human_player_info['player_robots']) ? count(explode(',', $human_player_info['player_robots'])) : 1;
+        $human_player_endgame_context = $this_battle->has_endgame_context() ? true : false;
+
+        // Reset the player's STAR SUPPORT cooldown, thus unlocking the feature, in teh correct context
+        //error_log('checking if the player is in the correct context to unlock Star Support');
+        //error_log('$this_player->player_autopilot = '.($this_player->player_autopilot ? 'true' : 'false'));
+        //error_log('$human_player_endgame_context = '.($human_player_endgame_context ? 'true' : 'false'));
+        if ($this_player->player_autopilot === true
+            && $human_player_endgame_context === true){
+
+            // Reset the cooldown for star support which will unlock it
+            if (!rpg_prototype::star_support_unlocked()){
+                //error_log('unlocking Star Support for the player');
+                $this_battle->set_flag('star_support_is_new', true);
+            } else {
+                //error_log('resetting Star Support cooldown for the player');
+                $this_battle->set_flag('star_support_is_new', false);
+            }
+            rpg_prototype::reset_star_support_cooldown();
+            rpg_prototype::set_star_support_cooldown(0);
+
+            // Print a message showing that this effect is taking place
+            $this_skill = $this_robot->get_skill_object(null);
+            $this_skill->skill_name = 'Final Dirge';
+            $event_trigger_options = array(
+                'this_skill' => $this_skill,
+                'canvas_show_this_skill_overlay' => false,
+                'canvas_show_this_skill_underlay' => true,
+                'event_flag_camera_action' => true,
+                'event_flag_camera_side' => $this_robot->player->player_side,
+                'event_flag_camera_focus' => $this_robot->robot_position,
+                'event_flag_camera_depth' => $this_robot->robot_key,
+                'event_flag_camera_offset' => 0
+                );
+            $this_robot->set_frame('summon');
+            $this_robot->set_frame_styles('filter: brightness(3); ');
+            $this_battle->queue_sound_effect('cosmic-sound');
+            $this_battle->queue_sound_effect(array('name' => 'boss-taunt-sound', 'delay' => 100));
+            $this_battle->events_create($this_robot, false, $this_robot->robot_name.'\'s '.$this_skill->skill_name,
+                $this_robot->print_name().' calls out for '.$this_robot->get_pronoun('possessive2').' master!<br />'.
+                $this_robot->print_quote('custom', null, null, 'Master Duo! Hear my cry!! Watashi o terashite kudasai!!!'),
+                $event_trigger_options
+                );
+            $this_robot->set_frame('summon');
+            $this_robot->set_frame_styles('filter: brightness(2); ');
+            $event_trigger_options['event_flag_camera_offset'] += 1;
+            $this_battle->queue_sound_effect(array('name' => 'boss-taunt-sound', 'delay' => 100));
+            $this_battle->events_create($this_robot, false, '', '', $event_trigger_options);
+            $this_robot->set_frame('defend');
+            $this_robot->reset_frame_styles();
+            $event_trigger_options['event_flag_camera_offset'] += 1;
+            $this_battle->queue_sound_effect(array('name' => 'boss-taunt-sound', 'delay' => 100));
+            $this_battle->events_create($this_robot, false, '', '', $event_trigger_options);
+            $this_robot->reset_frame();
+            $this_battle->queue_sound_effect('shining-sound');
+
+        }
 
         // Return true on success
         return true;
