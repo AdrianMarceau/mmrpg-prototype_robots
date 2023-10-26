c2b7e1fd541a212115874fe0acc69c94c7e012a5
MegaBossMan <megabossman50@gmail.com>
2023-10-26 00:09:33 -0400

    Updated Heat Man's data and functions

--- heat-man/functions.php
+++ heat-man/functions.php
@@ -8,6 +8,52 @@ $functions = array(
         // Return true on success
         return true;
 
+    },
+    'robot_function_onload' => function($objects){
+
+        // Extract all objects into the current scope
+        extract($objects);
+
+        // Collect the context variable for the battle for reference
+        $context = $this_battle->values['context']; // player, chapter, phase, etc.
+
+        // Return true on success
+        return true;
+
+    },
+    'robot_function_ontargetchange' => function($objects){
+
+        // Extract all objects into the current scope
+        extract($objects);
+
+        // Update this robot's taunt quote based on the robot being faced
+        $target_robot_info = rpg_robot::get_index_info($target_robot->robot_token);
+
+        // Otherwise if this is ANY OTHER TIME we can define our quotes based on specific characters
+        if (true) {
+            switch ($target_robot->robot_token){
+                case 'bass': {
+                    $this_robot->set_quote('battle_victory', '... Dude, chill. You\’re giving me a migraine.');
+                    break;
+                }
+                case 'time-man': {
+                    $this_robot->set_quote('battle_victory', 'Whoa, you look ticked….wanna come to my barbecue later?');
+                    break;
+                }
+                case 'fire-man': {
+                    $this_robot->set_quote('battle_victory', 'I dunno, man. Justice seems like a drag...');
+                    break;
+                }
+            }
+        }
+
+        // Return true on success
+        return true;
+
     }
 );
+$functions['robot_function_onturnstart'] = function($objects) use ($functions){
+    //error_log('onturnstart for robot w/ target '.$objects['target_robot']->robot_string);
+    return $functions['robot_function_ontargetchange']($objects, true);
+};
 ?>
