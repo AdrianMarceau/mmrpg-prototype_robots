5190adf5d2745c9ddb0390fb697abbabac1c6c12
MegaBossMan <megabossman50@gmail.com>
2023-10-26 00:09:33 -0400

    Updated Dive Man's data and functions

--- dive-man/functions.php
+++ dive-man/functions.php
@@ -8,6 +8,60 @@ $functions = array(
         // Return true on success
         return true;
 
+    },
+    'robot_function_onload' => function($objects){
+
+        // Extract all objects into the current scope
+        extract($objects);
+
+        // Collect the context variable for the battle for reference
+        $context = $this_battle->values['context']; // player, chapter, phase, etc.
+
+        // Return true on success
+        return true;
+
+    },
+    'robot_function_ontargetchange' => function($objects){
+
+        // Extract all objects into the current scope
+        extract($objects);
+
+        // Update this robot's taunt quote based on the robot being faced
+        $target_robot_info = rpg_robot::get_index_info($target_robot->robot_token);
+
+        // Otherwise if this is ANY OTHER TIME we can define our quotes based on specific characters
+        if (true) {
+            switch ($target_robot->robot_token){
+                case 'proto-man': {
+                    $this_robot->set_quote('battle_victory', 'You\’re a robot of great morals, Proto Man. I salute you.');
+                    break;
+                }
+                case 'hard-man': {
+                    $this_robot->set_quote('battle_victory', 'Not only are your morals respectable, but that\’s a beautiful iron jaw you got there!');
+                    break;
+                }
+                case 'knight-man': {
+                    $this_robot->set_quote('battle_victory', 'Even though you lost, a man who sticks to his moral code deserves respect nonetheless.');
+                    break;
+                }
+                case 'magnet-man': {
+                    $this_robot->set_quote('battle_victory', 'Tracking targets by magnet is intuitive, yes, but it\'s no substitute for managing to hit them with sheer skill alone.');
+                    break;
+                }
+                case 'splash-woman': {
+                    $this_robot->set_quote('battle_victory', 'Your singing shall never beckon me, siren! My duty comes above all else!');
+                    break;
+                }
+            }
+        }
+
+        // Return true on success
+        return true;
+
     }
 );
+$functions['robot_function_onturnstart'] = function($objects) use ($functions){
+    //error_log('onturnstart for robot w/ target '.$objects['target_robot']->robot_string);
+    return $functions['robot_function_ontargetchange']($objects, true);
+};
 ?>
