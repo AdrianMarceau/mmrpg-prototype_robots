fec008e9b10918131617028c687e13438dfd7bd0
MegaBossMan <megabossman50@gmail.com>
2023-11-03 23:24:52 -0400

    Updated Charge Man's data and functions

--- charge-man/functions.php
+++ charge-man/functions.php
@@ -8,6 +8,48 @@ $functions = array(
         // Return true on success
         return true;
 
+    },
+    'robot_function_onload' => function($objects){
+
+        // Extract all objects into the current scope
+        extract($objects);
+
+        // Collect the context variable for the battle for reference
+        $context = $this_battle->values['context']; // player, chapter, phase, etc.
+
+        // Return true on success
+        return true;
+
+    },
+    'robot_function_ontargetchange' => function($objects){
+
+        // Extract all objects into the current scope
+        extract($objects);
+
+        // Update this robot's taunt quote based on the robot being faced
+        $target_robot_info = rpg_robot::get_index_info($target_robot->robot_token);
+
+        // Otherwise if this is ANY OTHER TIME we can define our quotes based on specific characters
+        if (true) {
+            switch ($target_robot->robot_token){
+                case 'turbo-man': {
+                    $this_robot->set_quote('battle_victory', 'Confound the blasted automobile! Always in a great hurry, plaguing our poor streets!');
+                    break;
+                }
+                case 'plug-man': {
+                    $this_robot->set_quote('battle_victory', 'Boy, I don\'t give half a darn on what an \'anime\' is! Now scram!');
+                    break;
+                }
+            }
+        }
+
+        // Return true on success
+        return true;
+
     }
 );
+$functions['robot_function_onturnstart'] = function($objects) use ($functions){
+    //error_log('onturnstart for robot w/ target '.$objects['target_robot']->robot_string);
+    return $functions['robot_function_ontargetchange']($objects, true);
+};
 ?>
