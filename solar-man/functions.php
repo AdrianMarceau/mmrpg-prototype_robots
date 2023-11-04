1d3071d6bf0028960057e01453dc688834452fc4
MegaBossMan <megabossman50@gmail.com>
2023-11-03 23:24:53 -0400

    Updated Solar Man's data and functions

--- solar-man/functions.php
+++ solar-man/functions.php
@@ -8,6 +8,52 @@ $functions = array(
         // Return true on success
         return true;
 
+    },
+    'robot_function_onload' => function($objects){
+
+        // Extract all objects into the current scope
+        extract($objects);
+
+        // Collect the context variable for the battle for reference
+        $context = $this_battle->values['context']; // player, chapter, phase, etc.
+
+        // Return true on success
+        return true;
+
+    },
+    'robot_function_ontargetchange' => function($objects){
+
+        // Extract all objects into the current scope
+        extract($objects);
+
+        // Update this robot's taunt quote based on the robot being faced
+        $target_robot_info = rpg_robot::get_index_info($target_robot->robot_token);
+
+        // Otherwise if this is ANY OTHER TIME we can define our quotes based on specific characters
+        if (true) {
+            switch ($target_robot->robot_token){
+                case 'guts-man': {
+                    $this_robot->set_quote('battle_victory', 'Wow, you\’re strong for an old guy! Your cardio regiment must be wild!');
+                    break;
+                }
+                case 'skull-man': {
+                    $this_robot->set_quote('battle_victory', 'You took it too far, man! You\’ve worked yourself to the bone! ');
+                    break;
+                }
+                case 'pharaoh-man': {
+                    $this_robot->set_quote('battle_victory', 'Hey.. You should join my hot yoga class. ');
+                    break;
+                }
+            }
+        }
+
+        // Return true on success
+        return true;
+
     }
 );
+$functions['robot_function_onturnstart'] = function($objects) use ($functions){
+    //error_log('onturnstart for robot w/ target '.$objects['target_robot']->robot_string);
+    return $functions['robot_function_ontargetchange']($objects, true);
+};
 ?>
